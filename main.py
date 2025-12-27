a11e3cddce5159be3cc286192d990dd7df57a03f
Shreyas Ashtamkar <51398602+Shreyas-Ashtamkar@users.noreply.github.com>
2025-12-27 10:34:56 +0530

    Add email template and implement sending emails

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from os import getenv
 
 from mailersend import MailerSendClient, EmailBuilder
 
+from datetime import datetime
+
 ID, GROUP, STEP, DOMAIN, NAME, DESCRIPTION, DURATION = range(7)
 
 PROJECTS_BANK = getenv("PROJECTS_BANK")
@@ -30,16 +32,42 @@ while project[STEP] not in ["1", ""]:
     project = projects_table[random_row]
     
 print(f"Random Project Selected: {project}")
+
+with open("email-template.html", "r", encoding="utf-8") as file:
+    html_template = file.read()
+
+html_content = html_template.format_map(
+    {
+        "recipient_name": getenv("EMAIL_RECIPIENT_0_NAME"),
+        "id": project[ID],
+        "tags" : f"{(project[DOMAIN]+' Domain') if project[DOMAIN] else ''}{' · Group ' + project[GROUP] if project[GROUP] else ''}{' · Step ' + project[STEP] if project[STEP] else ''} · {project[DURATION]} {'Weeks' if project[DURATION]!='1' else 'Week'}",
+        "group": project[GROUP],
+        "step": project[STEP],
+        "domain": project[DOMAIN],
+        "name": project[NAME],
+        "description": project[DESCRIPTION],
+        "duration": project[DURATION],
+        "date": datetime.now().strftime("%B %d, %Y"),
+        "current_week": 1,
+        "sender_name": getenv("EMAIL_SENDER")
+    }
+)
+
+if getenv("DEBUG_MODE", "false").lower() == "false":
+    ms = MailerSendClient()
     
-ms = MailerSendClient()
+    email = (EmailBuilder()
+            .from_email(getenv("EMAIL_SENDER_ADDRESS"), getenv("EMAIL_SENDER"))
+            .to_many([{"email": getenv("EMAIL_RECIPIENT_0_ADDRESS"), "name": getenv("EMAIL_RECIPIENT_0_NAME")}])
+            .subject(f"New Project! 🚀 - #{project[ID]}")
+            .html(html_content)
+            .build())
 
-email = (EmailBuilder()
-         .from_email("info@domain.com", "Sender Name")
-         .to_many([{"email": "shreyas.ashtamkar18@gmail.com", "name": "Recipient"}])
-         .subject("Hello from MailerSend!")
-         .html("Hello World!")
-         .text("Hello World!")
-         .build())
+    response = ms.emails.send(email)
 
-response = ms.emails.send(email)
-print(f"Email sent: {response.message_id}")
+    print(f"Email sent: {response.to_dict()}")
+
+else:
+    print("Debug mode is ON. Writing email to debug-email.html")
+    with open("debug-email.html", "w", encoding="utf-8") as debug_file:
+        debug_file.write(html_content)
