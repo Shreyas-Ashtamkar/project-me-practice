81112427d5dbe7444d73e729865a4f797ef7cef6
Shreyas Ashtamkar <shreyu@programmer.net>
2026-01-05 00:43:37 +0530

    Refactor main.py to enhance project allocation logic and improve email content generation

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-import datetime
-from email_me_anything import select_random_row, build_html_content, send_email
+from datetime import datetime
+from projectmepractice import register_new_user, feed_all_projects, allocate_next_project_for_user
+from email_me_anything import build_html_content, send_email
 
 if __name__ == "__main__":
-    data = select_random_row(
-        csv_path="500ProjectsList.csv",
-        skip_header=True
-    )
+    feed_all_projects("500ProjectsList.csv")
+    user = register_new_user("Shreyas", "shreyas.ashtamkar18@gmail.com")
+    allocated_project = allocate_next_project_for_user(user_id=user["id"])
+    data = {
+        **allocated_project,
+        'current_week': "1",
+        'recipient_name': user['name'],
+        'sender_name': "Shreyas Asus Laptop",
+        'tags' : (
+            f"{allocated_project['domain']} Domain"
+            + (f" | Group {allocated_project['group_id']} | Step {allocated_project['group_part']}" if allocated_project['has_parts'] == 'True' else "")
+            + f" | {allocated_project['duration']}" + (" Week" if allocated_project['duration']=='1' else " Weeks")
+        )
+    }
     
-    data.update({
-        "sender_name": "Shreyas from Laptop",
-        "recipient_name": "Shreyas",
-        "current_week": 1,
-        "tags": f"{(data['Domain']+' Domain')}{' · Group ' + data['Group'] if data['Group'] else ''}{' · Step ' + data['Step'] if data['Step'] else ''} · {data['Weeks']} {'Weeks' if data['Weeks']!='1' else 'Week'}"
-    })
-    
+    print(data)
     html_content = build_html_content(
         template_path="email-template.html",
-        data=data,
+        data = data,
         variable_map={
-            "id": "ID",
-            "name": "Project Idea",
-            "description": "Description",
-            "tags": "tags",
-            "recipient_name": "recipient_name",
+            "id" : "id",
+            "name" : "title",
+            "description" : "description",
+            "tags" : "tags",
             "current_week": "current_week",
-            "sender_name": "sender_name"
+            "recipient_name": "recipient_name",
+            "sender_name": "sender_name",
         }
     )
     
+    with open("debug_email.html", "w") as f:
+        f.write(html_content)
+    
     status = send_email(
-        subject = datetime.datetime.now().strftime("%B %d, %Y"),
+        subject = datetime.now().strftime("%B %d, %Y"),
         html_content = html_content,
-        sender={"email": "laptop.shreyas@dev-master.in", "name": "Shreyas from Laptop"},
-        recipients=[{"email": "shreyas.ashtamkar18@gmail.com", "name": "Shreyas"}]
+        sender={"email": "laptop.shreyas@dev-master.in", "name": "Shreyas Asus Laptop"},
+        recipients=[{"email": user["email"], "name": user["name"]}]
     )
     
-    print(f"Email send status: {status}")
+    print("Email sent status:", status)
