7d31f80d21bfc7b5a5ad6695084ba3899254cdd8
Shreyas Ashtamkar <shreyu@programmer.net>
2026-01-14 13:35:50 +0530

    test file should read example_projects.csv

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 if __name__ == "__main__":
     import time, main
-    for _ in range(15):
+    if not main.db_initialized():
+        main.feed_all_projects("example_projects.csv")
+    for _ in range(6):
         main.run("Shreyas", "shreyas.ashtamkar18@gmail.com")
         time.sleep(1)
