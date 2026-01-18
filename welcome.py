810826075d4e9635a0037ecaede0dc6a9f0a4460
Shreyas Ashtamkar <shreyu@programmer.net>
2026-01-18 12:01:44 +0530

    feat: Refactor email greeting logic into welcome.py and streamline user onboarding process + cleanups

--- welcome.py
+++ welcome.py
@@ -1,44 +1,35 @@
 from datetime import datetime
+from typing import overload
 from email_me_anything import send_email
 
-from projectmepractice.types import UserType, ProjectType
-from projectmepractice import (
-    register_user, 
-    feed_all_projects, 
-    allocate_next_project_for_user, 
-    db_initialized,
-    build_html_content
-)
+from projectmepractice.types import UserType
+from projectmepractice import build_html_content, register_user
 
 from projectmepractice.const import (
-    PROD_MODE, 
-    EMAIL_SENDER_ADDRESS, 
-    EMAIL_SENDER_NAME
+    PROD_MODE,
+    EMAIL_SENDER_NAME,
+    EMAIL_SENDER_ADDRESS
 )
 
-from welcome import welcome_user
-
-def send_project_to_user(user:UserType, project:ProjectType) -> str:
+def send_greeting_to_user(user:UserType):
     data = {
-        **project.to_dict(),
         'current_week': "1",
         'recipient_name': user.name,
         'sender_name': EMAIL_SENDER_NAME,
         'date': datetime.now().strftime("%B %d, %Y")
     }
-    print(f"Project #{project.id} allocated to {user.name}")
+    print(f"Welcoming {user.name}.")
     html_content = build_html_content(
-        template_path="email-templates/project.html",
-        data = data,
-        variable_map={
-            "name" : "title"
-        }
+        template_path="email-templates/welcome.html",
+        data=data,
+        simple=True #Disable AI for greeting, as it's not implemented
     )
+    print("Built HTML template")
     if PROD_MODE:
         status = send_email(
             sender={"email": EMAIL_SENDER_ADDRESS, "name": EMAIL_SENDER_NAME},
             recipients=[{"email": user.email, "name": user.name}],
-            subject = "New Practice Project - " + datetime.now().strftime("%B %d, %Y"),
+            subject = "Welcome to Practice Me Project",
             html_content = html_content
         )
     else:
@@ -47,14 +38,13 @@ def send_project_to_user(user:UserType, project:ProjectType) -> str:
         status = {"status":"Skipped......", "message":"PROD mode is off"}
     return status
 
-def run(name:str, email:str):
+def welcome_user(name:str, email:str):
     user = register_user(name, email)
     if user.is_new:
-        welcome_user(name, email)
-    new_allocated_project = allocate_next_project_for_user(user=user)
-    status = send_project_to_user(user, new_allocated_project)
-    print(f"Email sending (project) to {name}:", status["status"])
-
+        status = send_greeting_to_user(user)
+        print(f"Email sending (greeting) to {user.name}:", status["status"])
+    else:
+        print("User Old")
 
 if __name__ == "__main__":
     from os import getenv
@@ -62,25 +52,20 @@ if __name__ == "__main__":
     from concurrent.futures import ThreadPoolExecutor
     
     subscribed_users_file = Path("subscribed_users.db.csv")
-    projects_file = Path("prod.projects.db.csv")
-    projects_file = projects_file if projects_file.exists() else Path("example_projects.csv")
     
     MUTI_THREADED_EXECUTION = getenv("MUTI_THREADED_EXECUTION", "false") == "true"
     
-    if not db_initialized():
-        feed_all_projects(projects_file)
-        
     if subscribed_users_file.exists():
         SUBSCRIBER_FILE_PRESENT = True
         NAME,EMAIL=0,1
         with open(subscribed_users_file, 'r') as input_file:
             recepients = input_file.readlines()[1:]
             SUBSCRIBER_FILE_PRESENT = len(recepients)>0
-                
+    
     if SUBSCRIBER_FILE_PRESENT:
         def process_recipient(recipient_line:str):
             recipient = recipient_line.split(",")
-            run(recipient[NAME], recipient[EMAIL].strip())
+            welcome_user(recipient[NAME], recipient[EMAIL].strip())
         
         if MUTI_THREADED_EXECUTION:
             with ThreadPoolExecutor(max_workers=5) as executor:
@@ -88,6 +73,3 @@ if __name__ == "__main__":
         else:
             for recipient in recepients:
                 process_recipient(recipient)
-            
-    else:
-        run("Shreyas", "shreyas.ashtamkar18@gmail.com")
